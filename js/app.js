592f0d23668edc437310bcad1fd78cd372ab041b
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-08-31 23:50:45 +0600

    update

--- js/app.js
+++ js/app.js
@@ -5,5 +5,24 @@ const getCountry = () =>{
     const url = `https://restcountries.eu/rest/v2/name/${searchText}`
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data[0]))
+    .then(data => loadCountry(data[0]))
+}
+
+const loadCountry = (data) =>{
+    console.log(data)
+    const countryContainer = document.getElementById('country-container');
+    const countryDiv = document.createElement('div');
+    countryDiv.classList.add('country-details');
+    countryDiv.innerHTML = `
+    <img src="${data.flag}" alt="">
+    <h2>name : ${data.name}</h2>
+    <h2>area : ${data.area}</h2>
+    <h4>population : ${data.population}</h4>
+    <h5>capital : ${data.capital}</h5>
+    <p>region : ${data.region}</p>
+    <p>currency code : ${data.currencies[0].code}</p>
+    <p>Currency name : ${data.currencies[0].name}</p>
+    <p>currency symbol : ${data.currencies[0].symbol}</p>
+    `
+    countryContainer.appendChild(countryDiv);
 }
